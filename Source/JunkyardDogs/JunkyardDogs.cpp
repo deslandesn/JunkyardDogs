e1e6102df21d9ecad90bf2ea62a3c5a84d10b928
deslandesn <deslandesn@live.com>
2024-05-12 23:03:02 -0400

    Initial commit

---
+++ Source/JunkyardDogs/JunkyardDogs.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JunkyardDogs.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JunkyardDogs, "JunkyardDogs" );
